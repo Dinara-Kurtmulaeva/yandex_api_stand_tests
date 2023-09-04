ab3948f35d58c11f06a9a15e977850ff5242f840
Dinara Kurtmulaeva <dinarakurtmulaeva@gmail.com>
2023-09-04 23:32:43 +0300

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,49 @@
+import configuration
+import requests
+import data
+
+# получить страницу с документацией API Яндекс Прилавка
+def get_docs():
+   return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+#response = get_docs()
+#print(response.status_code)
+
+
+# Получение логов Unils
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count": 20})
+
+#response = get_logs()
+#print(response.status_code)
+#print(response.headers)
+
+
+# получить информацию из таблицы базы данных: Utils
+def get_users_table():
+   return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+#response = get_users_table()
+#print(response.status_code)
+
+
+# Создание нового пользователя
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+#response = post_new_user(data.user_body);
+#print(response.status_code)
+#print(response.json())
+
+
+#  POST-запрос на поиск наборов по продуктам
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                         headers=data.headers)
+
+#response = post_products_kits(data.product_ids);
+#print(response.status_code)
+#print(response.json())
+
